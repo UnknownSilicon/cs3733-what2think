6eb2f75a2d6ae1b00a437735720c141ac09a8dcc
Evelyn Dube <ecdgirl15@gmail.com>
2020-11-21 16:18:01 -0500

    add tests

--- src/test/java/edu/wpi/modula3/what2think/TestRegisterUser.java
+++ src/test/java/edu/wpi/modula3/what2think/TestRegisterUser.java
@@ -7,14 +7,16 @@ import edu.wpi.modula3.what2think.http.RegisterResponse;
 import edu.wpi.modula3.what2think.model.User;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestRegisterUser extends LambdaTest{
 
     @Test
-    void testRegisterName(){
+    void testRegister(){
         RegisterUser ru = new RegisterUser();
         CreateChoice cc = new CreateChoice();
 
-        User user = new User("test", null);
+        User user = new User("test", "ew");
 
         String[] alts = new String[]{"abc", "123", "def"};
         CreateRequest cRequest = new CreateRequest("Test1", 5, alts);
@@ -23,6 +25,35 @@ public class TestRegisterUser extends LambdaTest{
         RegisterRequest req = new RegisterRequest(cResponse.getChoice().getId(), user);
         RegisterResponse response = ru.handleRequest(req, createContext("registerUser"));
 
+        //assert success in adding
+        assertEquals(200, response.getStatusCode());
 
+        req = new RegisterRequest(cResponse.getChoice().getId(), user);
+        response = ru.handleRequest(req, createContext("registerUser"));
+
+        //success in finding
+        assertEquals(200, response.getStatusCode());
+
+        user.setPassword("no");
+        req = new RegisterRequest(cResponse.getChoice().getId(), user);
+        response = ru.handleRequest(req, createContext("registerUser"));
+
+        //failure because wrong password
+        assertEquals(400, response.getStatusCode());
+        assertEquals("incorrect password", response.getError());
+
+        user.setName("blank");
+        user.setPassword("");
+        req = new RegisterRequest(cResponse.getChoice().getId(), user);
+        response = ru.handleRequest(req, createContext("registerUser"));
+
+        //success in adding
+        assertEquals(200, response.getStatusCode());
+
+        req = new RegisterRequest(cResponse.getChoice().getId(), user);
+        response = ru.handleRequest(req, createContext("registerUser"));
+
+        //success in finding
+        assertEquals(200, response.getStatusCode());
     }
 }
