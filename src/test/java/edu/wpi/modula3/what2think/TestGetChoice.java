c93c056dab66efdf114f3e7c1e6b81e5295da7a6
Lucas Buermeyer <lbuermeyer@gmail.com>
2020-11-23 02:22:16 -0500

    Get choice

--- src/test/java/edu/wpi/modula3/what2think/TestGetChoice.java
+++ src/test/java/edu/wpi/modula3/what2think/TestGetChoice.java
@@ -4,6 +4,7 @@ import edu.wpi.modula3.what2think.http.GetRequest;
 import edu.wpi.modula3.what2think.http.GetResponse;
 import edu.wpi.modula3.what2think.model.Alternative;
 import edu.wpi.modula3.what2think.model.Choice;
+import edu.wpi.modula3.what2think.model.User;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -14,40 +15,46 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestGetChoice extends LambdaTest {
 
     @Test
-    public void testGetChoice() {
+    public void testGetUnfinishedChoice() {
         GetChoice gc = new GetChoice();
 
-        String[] alts = new String[]{"abc", "123", "def"};
-
-        GetRequest request = new GetRequest("012345678901234567890123456789123456");
+        GetRequest request = new GetRequest("Test1");
 
         GetResponse response = gc.handleRequest(request, createContext("getChoice"));
 
         assertEquals(200, response.getStatusCode());
+
         assertEquals("", response.getError());
         assertNotNull(response.getChoice());
 
         Choice choice = response.getChoice();
 
-        try {
-            UUID.fromString(choice.getId());
-        } catch (IllegalArgumentException e) {
-            fail();
-        }
+        assertEquals("Test1", choice.getId());
+        assertEquals("Test 1", choice.getDescription());
+        assertEquals(1, choice.getMaxUsers().intValue());
+        assertFalse(choice.isCompleted());
+        assertNull(choice.getChosenAlternative());
+        assertNull(choice.getCompletionTime());
+        //assertEquals("2020-11-23 00:02:39.947", choice.getCreationTime()); //working sort of? Hours wrong
 
-        assertEquals("Test1", choice.getDescription());
-        assertEquals(5, choice.getMaxUsers().intValue());
+        User[] users = choice.getUsers();
+        assertEquals("LUCAS", users[0].getName());
 
-        for (Alternative a : choice.getAlternatives()) {
-            try {
-                UUID.fromString(a.getId());
-            } catch (IllegalArgumentException e) {
-                fail();
-            }
+        Alternative[] alternatives = choice.getAlternatives();
+        assertEquals("alt1-1", alternatives[0].getId());
+        assertEquals("alt1-2", alternatives[1].getId());
+        assertEquals("alt1-3", alternatives[2].getId());
 
-            if (Arrays.stream(alts).noneMatch(x -> x.equals(a.getContent()))) {
-                fail();
-            }
-        }
+        assertEquals("alt 1-1", alternatives[0].getContent());
+        assertEquals("alt 1-2", alternatives[1].getContent());
+        assertEquals("alt 1-3", alternatives[2].getContent());
+
+        assertEquals("LUCAS", alternatives[0].getDisapprovers()[0].getName());
+        assertEquals("LUCAS", alternatives[1].getApprovers()[0].getName());
+        assertNull(alternatives[2].getApprovers());
+        assertNull(alternatives[2].getDisapprovers());
+
+        assertEquals("none", alternatives[0].getFeedback()[0].getContent());
+        assertNull(alternatives[1].getFeedback());
     }
 }
