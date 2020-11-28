b549b18b4551a4f771fcd4638929d3f8cca1c5cd
Evelyn Dube <ecdgirl15@gmail.com>
2020-11-28 15:35:08 -0500

    disallow malformed input and add tests

--- src/test/java/edu/wpi/modula3/what2think/TestRegisterUser.java
+++ src/test/java/edu/wpi/modula3/what2think/TestRegisterUser.java
@@ -56,4 +56,57 @@ public class TestRegisterUser extends LambdaTest{
         //success in finding
         assertEquals(200, response.getStatusCode());
     }
+
+    @Test
+    void testInvalidInputs(){
+        RegisterUser ru = new RegisterUser();
+
+        CreateChoice cc = new CreateChoice();
+        String[] alts = new String[]{"abc", "123", "def"};
+        CreateRequest cRequest = new CreateRequest("Test1", 5, alts);
+        CreateResponse cResponse = cc.handleRequest(cRequest, createContext("createChoice"));
+
+        User legitUser = new User("invalidTesting", "");
+
+        RegisterRequest req = new RegisterRequest(null, legitUser);
+        RegisterResponse response = ru.handleRequest(req, createContext("registerUser"));
+
+        assertEquals(400, response.getStatusCode());
+        assertEquals("invalid input", response.getError());
+
+        req = new RegisterRequest("", legitUser);
+        response = ru.handleRequest(req, createContext("registerUser"));
+
+        assertEquals(400, response.getStatusCode());
+        assertEquals("invalid input", response.getError());
+
+        req = new RegisterRequest(cResponse.getChoice().getId(), null);
+        response = ru.handleRequest(req, createContext("registerUser"));
+
+        assertEquals(400, response.getStatusCode());
+        assertEquals("invalid input", response.getError());
+
+        User badUser = new User(null, "invalidTesting");
+
+        req = new RegisterRequest(cResponse.getChoice().getId(), badUser);
+        response = ru.handleRequest(req, createContext("registerUser"));
+
+        assertEquals(400, response.getStatusCode());
+        assertEquals("invalid input", response.getError());
+
+        badUser.setName("");
+        req = new RegisterRequest(cResponse.getChoice().getId(), badUser);
+        response = ru.handleRequest(req, createContext("registerUser"));
+
+        assertEquals(400, response.getStatusCode());
+        assertEquals("invalid input", response.getError());
+
+        badUser.setName("invalidTesting");
+        badUser.setPassword(null);
+        req = new RegisterRequest(cResponse.getChoice().getId(), badUser);
+        response = ru.handleRequest(req, createContext("registerUser"));
+
+        assertEquals(400, response.getStatusCode());
+        assertEquals("invalid input", response.getError());
+    }
 }
